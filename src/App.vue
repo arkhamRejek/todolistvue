27a42691bba9fc1784c914dc8600c02199f4e67b
Paul-E <paulbrunache@gmail.com>
2020-11-19 18:31:54 -0500

    assignment implement logout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="container">
+    <nav>logout</nav>
     <router-view></router-view>
   </div>
 </template>
@@ -8,6 +9,9 @@
 export default {
   async created() {
     await this.$store.dispatch("app/init");
+  },
+  methods: {
+    logout() {},
   },
 };
 </script>
